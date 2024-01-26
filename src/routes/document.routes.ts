6daf7f65be39d0b98777be53c6e1de7c1a23c054
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-01-26 23:53:45 +0200

    Alenmak

--- src/routes/document.routes.ts
+++ src/routes/document.routes.ts
@@ -17,7 +17,7 @@ router.get("/:id", validateToken, getDocumentById);
 router.get("/owner/:id", validateToken, getDocumentByOwnerId);
 router.post("/:owner/create", validateToken, createDocument);
 router.delete("/delete/:id", validateToken, deleteDocument);
-router.get("/download/:fileName", validateToken, downloadDocument);
-router.get("/preview/:fileName", validateToken, getPreviewLink);
+router.get("/download/:id", validateToken, downloadDocument);
+router.get("/preview/:id", validateToken, getPreviewLink);
 
 export default router;
